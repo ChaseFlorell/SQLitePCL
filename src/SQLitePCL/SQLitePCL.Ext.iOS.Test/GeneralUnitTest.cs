8098ced351d09e44c2db233a265c14dfa2c1ce12
Matthew Leibowitz <mattleibow@live.com>
2014-04-14 13:56:47 +0200

    corrected the namespace

--- src/SQLitePCL/SQLitePCL.Ext.iOS.Test/GeneralUnitTest.cs
+++ src/SQLitePCL/SQLitePCL.Ext.iOS.Test/GeneralUnitTest.cs
@@ -7,7 +7,7 @@
 // 
 // See the Apache 2 License for the specific language governing permissions and limitations under the License.
 
-namespace SQLitePCL.Ext.Android.Test
+namespace SQLitePCL.Ext.iOS.Test
 {
 	using NUnit.Framework;
 	using TestClassAttribute = NUnit.Framework.TestFixtureAttribute;
