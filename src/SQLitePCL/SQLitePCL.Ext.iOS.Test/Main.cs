50f0dedccc173ed9df937e360811f65b89d49b4a
Matthew Leibowitz <mattleibow@live.com>
2014-04-14 11:12:51 +0200

    Added the Xamarin.iOS bindings for the SQLitePCL.

---
+++ src/SQLitePCL/SQLitePCL.Ext.iOS.Test/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace SQLitePCL.Ext.iOS.Test
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main(string[] args)
+		{
+// ReSharper disable UnusedVariable
+			// init the  buts to stop the linker removing assemblies
+			var platform = SQLite3Provider.Instance;
+// ReSharper restore UnusedVariable
+
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main(args, null, "AppDelegate");
+		}
+	}
+}
