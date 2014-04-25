40d99e3b0e2c6cee93d5e603111768ef17e1031c
Leonardo M. Tilli <leotilli@gmail.com>
2014-04-25 17:21:04 -0300

    Update Assembly Info version

--- src/SQLitePCL/SQLitePCL.Ext.iOS/Properties/AssemblyInfo.cs
+++ src/SQLitePCL/SQLitePCL.Ext.iOS/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using MonoTouch.Foundation;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.8.4.2")]
-[assembly: AssemblyFileVersion("3.8.4.2")]
+[assembly: AssemblyVersion("3.8.4.3")]
+[assembly: AssemblyFileVersion("3.8.4.3")]
 
 [assembly: Preserve(AllMembers = true)]
